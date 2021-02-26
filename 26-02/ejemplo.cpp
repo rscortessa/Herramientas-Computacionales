38a232cf195bc912cf7f8de5d72f29d0ca7a976b
Ronalito <rscortessa@unal.edu.co>
2021-02-26 12:37:14 -0500

    Cambios hechos en clase

--- 26-02/ejemplo.cpp
+++ 26-02/ejemplo.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 int main(int argc, char **argv)
 {
-std::cout<<"Hola mundo \n";
+  int a=std::atoi(argv[1]); //Imprime lo que le doy en la linea de comandos.
+std::cout<<"Hola mundo \n"; 
+ std::cout<<"a "<<a<<std::endl;
   return 0;
 }
